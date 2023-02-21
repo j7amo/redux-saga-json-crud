3208ebe124627138d74c847352fe0a2235f22d04
Roman Tarnakin <roman_tarnakin@epam.com>
2023-02-21 10:40:20 +0300

    add About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { MDBTypography } from 'mdb-react-ui-kit';
 
 function About() {
   return (
-    <div>
-      <h2>About</h2>
+    <div style={{ marginTop: '100px' }}>
+      <MDBTypography note noteColor="primary">
+        This is a React + Redux application. CRUD operations are implemented via
+        Redux Saga.
+      </MDBTypography>
     </div>
   );
 }
